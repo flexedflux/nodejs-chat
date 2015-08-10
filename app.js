9f694c7cf9c14c6aebb405b10861541c4acd29df
Igor Cheliadinski <flexedflux@icloud.com>
2015-08-10 20:08:27 +0300

    Added logger and view templates

--- app.js
+++ app.js
@@ -1,38 +1,35 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var config = require('./config');
+var log = require('./libs/log')(module);
 
 var app = express();
-app.set('port', 3000);
+app.set('port', config.get('port'));
+app.set('views', __dirname + '/templates');
+app.set('view engine', 'ejs');
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+app.use(express.favicon());
+if (app.get('env') === 'development') {
+	app.use(express.logger('dev'));
+} else {
+	app.use(express.logger('default'));
+}
 
-app.use(function (req, res, next) {
-	if (req.url === '/') {
-		res.end('hello');
-	} else {
-		next();
-	}
-});
-app.use(function (req, res, next) {
-	if (req.url === '/test') {
-		res.end('Test');
-	} else {
-		next();
-	}
-});
-app.use(function (req, res, next) {
-	if (req.url === '/forbidden') {
-		next(new Error('Access denied!'));
-	} else {
-		next();
-	}
-});
-app.use(function (req, res) {
-	res.send(404, 'Page Not Found');
-});
+app.use(express.bodyParser());
+
+app.use(express.cookieParser());
+
+app.use(app.router);
+
+app.get('/', function (req, res, next) {
+	res.render('index', {
+		body: '<b>Hello World</b>'
+	});
+})
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(function (err, req, res, next) {
 	if (app.get('env') === 'development') {
 		var errorHandler = express.errorHandler();
@@ -40,4 +37,10 @@ app.use(function (err, req, res, next) {
 	} else {
 		res.send(500, 'Internal Server Error!');
 	}
+});
+
+http
+	.createServer(app)
+	.listen(config.get('port'), function(){
+  	log.info('Express server listening on port ' + config.get('port'));
 });
